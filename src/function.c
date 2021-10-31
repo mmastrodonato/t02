97a5b0ce14f31c5b4e17b5e81994927b7f37fde3
Matteo <matteo.mastrodonato@gmail.com>
2021-10-31 12:18:52 +0100

    Nuovo file function

---
+++ src/function.c
@@ -0,0 +1,17 @@
+#include "prova.h"
+
+/* Primo SW al mondo! */
+int main (int){
+	
+	print ("Hello world");
+	
+	return 0;
+	
+}
+
+/* Somma */
+int somma (int add1, int add2){
+	
+	return (add1 + add2);
+	
+}
